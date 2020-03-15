41c283c6c2a2093e036e6303543fdb5ffd6097cc
Jin <jinwoov@gmail.com>
2020-03-15 16:48:59 -0700

    finished couple lessons

--- First Project/Assets/NewBehaviourScript.cs
+++ First Project/Assets/NewBehaviourScript.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public int maximum;
+    public int minimum;
+    public int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-        int maximum = 1000;
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        int guess = 500;
         int minimum = 1;
-
-
+        int maximum = 1000;
         Debug.Log("Hello friend, welcome to number wizard, Yo");
         Debug.Log("Please a pick a number");
         Debug.Log($"Lowest number you can pick is: {minimum}");
         Debug.Log($"Highest number you can pick is: {maximum}");
         Debug.Log("Tell me if your number is higher or lower than 500");
         Debug.Log("Push up = higher, Push Down = Lower, Push Enter = Corrent");
+        maximum += 1;
     }
 
     // Update is called once per frame
@@ -24,15 +34,25 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
+            minimum = guess;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
+            maximum = guess;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Return key was pressed.");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (maximum + minimum) / 2;
+        Debug.Log($"Is it higher or lower than {guess}");
+
     }
 }
