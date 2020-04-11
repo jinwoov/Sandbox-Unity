b017a2c2cd6249578cff2f3d7c6a7e7e00f253ca
Jin <jinwoov@gmail.com>
2020-04-10 23:54:11 -0700

    gg

---
+++ Star-war-hunts/Assets/AddVentureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddVentureGame : MonoBehaviour
+{
+    [SerializeField] Text TextComponent;  // available in inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextComponent.text = "Its the mochi, underwater goachi";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
